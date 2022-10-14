98ad4b56298b7fbca4a4986810fdf435f50148dc
dimon-filatov <67680759+dimon-filatov@users.noreply.github.com>
2022-10-14 18:55:27 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,2 +1,4 @@
 def test_link_with_language(browser, language):
     browser.get(f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/')
+    button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form button')
+    assert button
